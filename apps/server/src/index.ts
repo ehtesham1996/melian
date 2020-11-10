aa13f851bad9d3bea5d8f0606801763b062989eb
Marcony Neves <marcony.neves@cvortex.io>
2020-11-09 23:13:14 -0300

    Add json-server rest mock server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+	console.log("JSON Server is running : http://localhost:3000");
+});
