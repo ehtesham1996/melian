567ae1f08ddc79e067ec79c68c80ebfd7d1bfada
Marcony Neves <marcony.neves@cvortex.io>
2020-11-12 11:59:28 -0300

    ML-78-Adicionar hook customizado para file upload

---
+++ apps/web/src/hooks/useFileUpload.ts
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const useFileUpload = () => {
+	const [files, setFiles] = useState<any[]>();
+
+	const getFileMetadata = (file: any) =>
+		/**
+		 * The way we are handling uploads does not allow us to
+		 * turn the uploaded [object File] into JSON.
+		 *
+		 * Therefore, we have to write our own "toJSON()" method.
+		 */
+		({
+			lastModified: file.lastModified,
+			name: file.name,
+			size: file.size,
+			type: file.type,
+			webkitRelativePath: file.webkitRelativePath,
+		});
+
+	const fileToBase64 = async (file: any) =>
+		new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => resolve(reader.result);
+			reader.onerror = (e) => reject(e);
+		});
+
+	const handleUpload = async (e: any) => {
+		let newstate: any[] = [];
+		for (let i = 0; i < e.target.files.length; i++) {
+			const file = e.target.files[i];
+
+			const metadata = getFileMetadata(file);
+			const url = URL.createObjectURL(file);
+			const image64 = await fileToBase64(file);
+			newstate = [...newstate, { url, metadata, image64 }];
+		}
+		setFiles(newstate);
+	};
+
+	return {
+		files,
+		handleUpload,
+	};
+};
+
+export default useFileUpload;
