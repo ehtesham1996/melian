c7193e005023eadf13e496fd84d6125f3d7a264b
Marcony Neves <marcony.neves@cvortex.io>
2020-11-20 17:43:59 -0300

    ML-66-Update heroku execution

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,8 +5,10 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 8080;
+
 server.use(middlewares);
 server.use(router);
-server.listen(8080, () => {
-	console.log("JSON Server is running : http://localhost:8080");
+server.listen(PORT, () => {
+	console.log(`JSON Server is running : http://localhost:${PORT}`);
 });
