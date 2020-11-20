ce40fe4e8986ee08315ca9a4edfca1c894da2081
Marcony Neves <marcony.neves@cvortex.io>
2020-11-20 17:31:59 -0300

    ML-66-Fix axios cors error

--- apps/web/src/common/utils/axios.ts
+++ apps/web/src/common/utils/axios.ts
@@ -4,4 +4,8 @@ const axiosInstance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
 });
 
+axiosInstance.defaults.headers.post["Content-Type"] =
+	"application/json;charset=utf-8";
+axiosInstance.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
 export default axiosInstance;
